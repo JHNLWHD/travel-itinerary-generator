af1568f325bec31ff74539b2af4aff13f8c19719
JHNLWHD <aljhoenilw@gmail.com>
2023-02-08 23:00:38 +0800

    feat: Implement Travel Itinerary Generator MVP

---
+++ src/pages/api/travel-itinerary.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from "openai";
+import { env } from "../../env.mjs";
+
+// `env` is fully typesafe and provides autocompletion
+const dbUrl = env.DATABASE_URL;
+ interface TravelItinerary {
+  place: string
+  numberOfDays: number
+  numberOfPeople: number
+}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { place, numberOfDays, numberOfPeople } = req.body as TravelItinerary
+
+    const prompt = `Generate a travel itinerary for ${numberOfDays} days travel in ${place} with ${numberOfPeople} people`
+
+    const configuration = new Configuration({
+      apiKey: env.OPEN_API_KEY as string,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.7,
+      max_tokens: 4000,
+    });
+
+    res.status(response.status).json(response.data)
+  }
+}
