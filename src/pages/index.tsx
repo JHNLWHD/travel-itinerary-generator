98d75ceb9b6d997bb8084f4b4b1c03f64b81df2c
JHNLWHD <aljhoenilw@gmail.com>
2023-02-09 23:50:34 +0800

    fix: Update label color to always white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,19 +55,19 @@ const Home: NextPage = () => {
           </h1>
           <form className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-8" onSubmit={handleOnSubmit}>
             <div className="mb-6">
-              <label htmlFor="numberOfDays" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of days</label>
+              <label htmlFor="numberOfDays" className="block mb-2 text-sm font-medium text-white">Number of days</label>
               <input type="number" id="numberOfDays"
                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
 
             <div className="mb-6">
-              <label htmlFor="place" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location/Place</label>
+              <label htmlFor="place" className="block mb-2 text-sm font-medium text-white">Location/Place</label>
               <input type="text" id="place"
                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
 
             <div className="mb-6">
-              <label htmlFor="numberOfPeople" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of people</label>
+              <label htmlFor="numberOfPeople" className="block mb-2 text-sm font-medium text-white">Number of people</label>
               <input type="number" id="numberOfPeople"
                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
